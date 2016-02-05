b1e0a90dd9bc186fad2aa95624b2d81704bc173e
Gradient12 <pengwang.summer@gmail.com>
2016-02-05 10:33:49 -0500

    Corrected a mistake. Lab 1 and 2 completed.

--- lab/lab2/part4-underscore-refactor.js
+++ lab/lab2/part4-underscore-refactor.js
@@ -68,7 +68,7 @@
 
   var AddToMap = function(school){
     var color;
-    for (var i = 0; i < filtered_data.length - 1; i++) {
+
       isOpen = school.ACTIVE.toUpperCase() == 'OPEN';
       isPublic = (school.TYPE.toUpperCase() !== 'CHARTER' ||
                   school.TYPE.toUpperCase() !== 'PRIVATE');
@@ -88,7 +88,7 @@
       L.circleMarker([school.Y, school.X], pathOpts)
         .bindPopup(school.FACILNAME_LABEL)
         .addTo(map);
-    }
+
   };
 
   // clean data
@@ -97,36 +97,10 @@
   // filter data
   var filtered_data = _.filter(schools,FilterData);
 
-
   console.log('Included:', filtered_data.length);
   console.log('Excluded:', schools.length-filtered_data.length);
 
   // main loop
   _.each(filtered_data,AddToMap);
-
-/*
-  var color;
-  for (var i = 0; i < filtered_data.length - 1; i++) {
-    isOpen = filtered_data[i].ACTIVE.toUpperCase() == 'OPEN';
-    isPublic = (filtered_data[i].TYPE.toUpperCase() !== 'CHARTER' ||
-                filtered_data[i].TYPE.toUpperCase() !== 'PRIVATE');
-    meetsMinimumEnrollment = filtered_data[i].ENROLLMENT > minEnrollment;
-
-    // Constructing the styling  options for our map
-    if (filtered_data[i].HAS_HIGH_SCHOOL){
-      color = '#0000FF';
-    } else if (filtered_data[i].HAS_MIDDLE_SCHOOL) {
-      color = '#00FF00';
-    } else {
-      color = '##FF0000';
-    }
-    // The style options
-    var pathOpts = {'radius': filtered_data[i].ENROLLMENT / 30,
-                    'fillColor': color};
-    L.circleMarker([filtered_data[i].Y, filtered_data[i].X], pathOpts)
-      .bindPopup(filtered_data[i].FACILNAME_LABEL)
-      .addTo(map);
-  }
-*/
 
 })();
